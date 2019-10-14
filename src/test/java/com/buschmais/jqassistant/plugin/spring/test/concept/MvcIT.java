a31fed733420d87e00313abdbacd806c1b849c38
Dirk Mahler <dirk.mahler@buschmais.com>
2019-10-14 07:26:05 +0200

    #28 added concept for Spring MVC RestController

---
+++ src/test/java/com/buschmais/jqassistant/plugin/spring/test/concept/MvcIT.java
@@ -0,0 +1,29 @@
+package com.buschmais.jqassistant.plugin.spring.test.concept;
+
+import com.buschmais.jqassistant.plugin.spring.test.set.components.RestController;
+import com.buschmais.jqassistant.plugin.spring.test.set.components.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static com.buschmais.jqassistant.core.analysis.api.Result.Status.FAILURE;
+import static com.buschmais.jqassistant.core.analysis.api.Result.Status.SUCCESS;
+import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertThat;
+
+public class MvcIT extends AbstractSpringIT {
+
+    @Test
+    public void restController() throws Exception {
+        scanClasses(Service.class);
+        assertThat(applyConcept("spring-mvc:RestController").getStatus(), equalTo(FAILURE));
+        clearConcepts();
+        scanClasses(RestController.class);
+        assertThat(applyConcept("spring-mvc:RestController").getStatus(), equalTo(SUCCESS));
+        store.beginTransaction();
+        assertThat(query("MATCH (c:Spring:RestController:Controller) RETURN c").getColumn("c"), hasItem(typeDescriptor(RestController.class)));
+        store.commitTransaction();
+    }
+
+}
---
+++ src/test/java/com/buschmais/jqassistant/plugin/spring/test/set/components/RestController.java
@@ -0,0 +1,5 @@
+package com.buschmais.jqassistant.plugin.spring.test.set.components;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+}
