8f4c1832e78764837e03d03574aae4f9ddf16a5d
Dirk Mahler <dirk.mahler@buschmais.com>
2019-10-18 13:55:40 +0200

    removed concept "spring-component:RestController" and added IT for "spring-injection:Injectable"

--- src/test/java/com/buschmais/jqassistant/plugin/spring/test/concept/InjectableIT.java
+++ src/test/java/com/buschmais/jqassistant/plugin/spring/test/concept/InjectableIT.java
@@ -3,6 +3,7 @@ package com.buschmais.jqassistant.plugin.spring.test.concept;
 import java.util.List;
 
 import com.buschmais.jqassistant.plugin.java.api.model.ConstructorDescriptor;
+import com.buschmais.jqassistant.plugin.spring.test.set.components.*;
 import com.buschmais.jqassistant.plugin.spring.test.set.fieldinjection.ServiceWithConstructorInjection;
 import com.buschmais.jqassistant.plugin.spring.test.set.fieldinjection.ServiceWithFieldInjection;
 import com.buschmais.jqassistant.plugin.spring.test.set.injectables.ConfigurationBean;
@@ -40,10 +41,32 @@ public class InjectableIT extends AbstractSpringIT {
         store.beginTransaction();
         List<ConstructorDescriptor> constructors = query("MATCH (c:Constructor:Spring:InjectionPoint) RETURN c").getColumn("c");
         assertThat(constructors.size(), equalTo(1));
-//        assertThat(constructors, (Matcher<? super List<ConstructorDescriptor>>) hasItem(constructorDescriptor(ServiceWithConstructorInjection.class, Repository.class)));
+        // assertThat(constructors, (Matcher<? super List<ConstructorDescriptor>>)
+        // hasItem(constructorDescriptor(ServiceWithConstructorInjection.class,
+        // Repository.class)));
         List<ConstructorDescriptor> fields = query("MATCH (f:Field:Spring:InjectionPoint) RETURN f").getColumn("f");
         assertThat(fields.size(), equalTo(1));
-//        assertThat(fields, (Matcher<? super List<ConstructorDescriptor>>) hasItem(fieldDescriptor(ServiceWithFieldInjection.class, "repository")));
+        // assertThat(fields, (Matcher<? super List<ConstructorDescriptor>>)
+        // hasItem(fieldDescriptor(ServiceWithFieldInjection.class, "repository")));
         store.commitTransaction();
     }
+
+    @Test
+    public void injectable() throws Exception {
+        scanClasses(ConfigurationWithBeanProducer.class, AnnotatedRepository.class, ImplementedRepository.class, Controller.class, RestController.class,
+                Service.class);
+        assertThat(applyConcept("spring-injection:Injectable").getStatus(), equalTo(SUCCESS));
+        store.beginTransaction();
+        List<Object> injectables = query("MATCH (i:Type:Spring:Injectable) RETURN i").getColumn("i");
+        assertThat(injectables.size(), equalTo(7));
+        assertThat(injectables, hasItem(typeDescriptor(ConfigurationBean.class)));
+        assertThat(injectables, hasItem(typeDescriptor(ConfigurationWithBeanProducer.class)));
+        assertThat(injectables, hasItem(typeDescriptor(AnnotatedRepository.class)));
+        assertThat(injectables, hasItem(typeDescriptor(ImplementedRepository.class)));
+        assertThat(injectables, hasItem(typeDescriptor(Controller.class)));
+        assertThat(injectables, hasItem(typeDescriptor(RestController.class)));
+        assertThat(injectables, hasItem(typeDescriptor(Service.class)));
+        store.commitTransaction();
+    }
+
 }
