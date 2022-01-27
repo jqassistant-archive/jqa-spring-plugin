0a896f2f01c1e883b241a468685cb463fcbdd67e
Dirk Mahler <dirk.mahler@buschmais.com>
2022-01-27 17:41:39 +0100

    Removed deprecated MapBuilder

--- src/test/java/com/buschmais/jqassistant/plugin/spring/test/constraint/AllTypesInApplicationPackageIT.java
+++ src/test/java/com/buschmais/jqassistant/plugin/spring/test/constraint/AllTypesInApplicationPackageIT.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import com.buschmais.jqassistant.core.report.api.model.Result;
 import com.buschmais.jqassistant.core.rule.api.model.Constraint;
-import com.buschmais.jqassistant.plugin.common.test.scanner.MapBuilder;
+import com.buschmais.jqassistant.core.shared.map.MapBuilder;
 import com.buschmais.jqassistant.plugin.java.api.model.TypeDescriptor;
 import com.buschmais.jqassistant.plugin.java.test.AbstractJavaPluginIT;
 import com.buschmais.jqassistant.plugin.spring.test.set.application.invalid.Controller;
@@ -49,7 +49,7 @@ class AllTypesInApplicationPackageIT extends AbstractJavaPluginIT {
 
     private void deletePackages(Class<?> rootClass) {
         store.beginTransaction();
-        Map<String, Object> params = MapBuilder.<String, Object> create("package", rootClass.getPackage().getName()).get();
+        Map<String, Object> params = MapBuilder.<String, Object> builder().entry("package", rootClass.getPackage().getName()).build();
         store.executeQuery("MATCH (p:Package) WHERE NOT p.fqn starts with {package} DETACH DELETE p", params);
         store.commitTransaction();
         // Clear the TX cache as the ids of the deleted nodes will be re-used by Neo4j when creating new nodes
